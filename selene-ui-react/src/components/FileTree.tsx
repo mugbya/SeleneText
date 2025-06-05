719153e8cac850548b597958c6deb891c5dd659c
mugbya <mugbya@gmail.com>
2025-06-05 23:31:00 +0800

    feat:优化树形结构
    Signed-off-by: mugbya <mugbya@gmail.com>

--- selene-ui-react/src/components/FileTree.tsx
+++ selene-ui-react/src/components/FileTree.tsx
@@ -1,3 +1,7 @@
+import { useState } from "react";
+import { File, Folder, FolderOpen } from "lucide-react";
+import { cn } from "@/lib/utils"; // shadcn 推荐的 class 合并函数
+
 type FileNode = {
     name: string;
     path: string;
@@ -7,13 +11,47 @@ type FileNode = {
 
 export default function FileTree({ nodes }: { nodes: FileNode[] }) {
     return (
-        <ul>
+        <ul className="pl-2 text-sm space-y-1">
             {nodes.map((node) => (
-                <li key={node.path}>
-                    {node.isDirectory ? '📁' : '📄'} {node.name}
-                    {node.children && <FileTree nodes={node.children} />}
-                </li>
+                <TreeNode key={node.path} node={node} />
             ))}
         </ul>
+    );
+}
+
+function TreeNode({ node }: { node: FileNode }) {
+    const [expanded, setExpanded] = useState(false);
+    const isDir = node.isDirectory;
+
+    const handleToggle = () => {
+        if (isDir) setExpanded((prev) => !prev);
+    };
+
+    return (
+        <li>
+            <div
+                onClick={handleToggle}
+                className={cn(
+                    "flex items-center gap-1 px-2 py-1 rounded-md cursor-pointer",
+                    "hover:bg-zinc-100 dark:hover:bg-zinc-800"
+                )}
+                title={node.path}
+            >
+                {isDir ? (
+                    expanded ? <FolderOpen className="w-4 h-4 text-yellow-500" /> : <Folder className="w-4 h-4 text-yellow-500" />
+                ) : (
+                    <File className="w-4 h-4 text-zinc-500" />
+                )}
+                <span className="truncate">{node.name}</span>
+            </div>
+
+            {isDir && expanded && node.children && (
+                <ul className="pl-4 border-l border-zinc-300 dark:border-zinc-700 ml-1">
+                    {node.children.map((child) => (
+                        <TreeNode key={child.path} node={child} />
+                    ))}
+                </ul>
+            )}
+        </li>
     );
 }
--- selene-ui-react/src/modules/LeftPanel.tsx
+++ selene-ui-react/src/modules/LeftPanel.tsx
@@ -1,40 +1,43 @@
-import { useEffect, useState } from 'react';
-// import {FileTree} from "./components/";
+import { useEffect, useState } from "react";
 import FileTree from "@/components/FileTree";
-const { ipcRenderer } = window.require('electron');
+
+const { ipcRenderer } = window.require("electron");
 
 function LeftPanel() {
-  const [folders, setFolders] = useState<any[]>([]);
+  const [folders, setFolders] = useState<
+      { basePath: string; contents: any[] }[]
+  >([]);
 
   useEffect(() => {
-    ipcRenderer.on('replace-folders', (event, { basePath, contents }) => {
+    ipcRenderer.on("replace-folders", (event, { basePath, contents }) => {
       setFolders([{ basePath, contents }]);
     });
 
-    ipcRenderer.on('append-folder', (event, { basePath, contents }) => {
-      setFolders(prev => [...prev, { basePath, contents }]);
+    ipcRenderer.on("append-folder", (event, { basePath, contents }) => {
+      setFolders((prev) => [...prev, { basePath, contents }]);
     });
 
     return () => {
-      ipcRenderer.removeAllListeners('replace-folders');
-      ipcRenderer.removeAllListeners('append-folder');
+      ipcRenderer.removeAllListeners("replace-folders");
+      ipcRenderer.removeAllListeners("append-folder");
     };
   }, []);
 
   return (
-    <div className="left-panel">
-      {folders.map((folder, i) => (
-        <div key={i} className="folder">
-          <h4>{folder.basePath}</h4>
-          <FileTree  nodes={folder.contents} />
-          {/*<ul>*/}
-          {/*  {folder.contents.map(item => (*/}
-          {/*    <li key={item.path}>{item.name}{item.isDirectory ? '/' : ''}</li>*/}
-          {/*  ))}*/}
-          {/*</ul>*/}
-        </div>
-      ))}
-    </div>
+      <div className="left-panel p-2 space-y-2">
+        {folders.map((folder, i) => {
+          const rootName = folder.basePath.split("/").filter(Boolean).pop();
+
+          const treeRoot = {
+            name: rootName,
+            path: folder.basePath,
+            isDirectory: true,
+            children: folder.contents,
+          };
+
+          return <FileTree key={folder.basePath} nodes={[treeRoot]} />;
+        })}
+      </div>
   );
 }
 
