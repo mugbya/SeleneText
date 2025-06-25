2bc39d2b3b7ab3f25dbe638c6a817afa61058641
mugbya <mugbya@gmail.com>
2025-06-25 16:48:21 +0800

    fix:解决获取最新项目不是最新+代码优化
    Signed-off-by: mugbya <mugbya@gmail.com>

--- selene-ui-react/src/logic/fileSaver.ts
+++ selene-ui-react/src/logic/fileSaver.ts
@@ -1,27 +1,21 @@
-import {FileTab} from "@/types";
+import { FileTab } from "@/types";
 
 export async function handleFileSave(
     current: FileTab | undefined,
-    updateFile: (oldPath: string, updated: FileTab) => void
 ) {
-    // const projects = useProjectsStore((s) => s.projects);
-    // const removeOrphanFile = useProjectsStore((s) => s.removeOrphanFile);
-
-    console.log("handleFileSave: ", current, updateFile );
+    // console.log("handleFileSave: ", current, updateFile);
     if (!current) {
         console.warn("❌ handleFileSave 调用失败：current is null or undefined");
         return;
     }
-
     // const hasSaved = current.path && !current.path.startsWith("Untitled-");
-    console.log("current.isTemporary: ", current.isTemporary);
+    // console.log("current.isTemporary: ", current.isTemporary);
     const hasSaved = !current.isTemporary;
-
 
     console.log("hasSaved: ", hasSaved);
     if (hasSaved) {
         // ✅ 直接保存到原路径
-        const res = await window.electronAPI.saveFile(current.path, current.content)
+        await window.electronAPI.saveFile(current.path, current.content)
         return;
     }
 
@@ -29,26 +23,7 @@ export async function handleFileSave(
     const res = await window.electronAPI.saveFileAs("untitled.txt", current.content)
 
     if (res.success && res.filePath) {
-        const updated = { ...current, path: res.filePath };
-        updateFile(current.path, updated);
-
-            // ✅ 自动添加一个新项目（如果这个文件不在已有项目中）
-            // const basePath = path.dirname(res.filePath); // 从文件路径中提取文件夹路径
-            // const basePath = res.filePath; // 从文件路径中提取文件夹路径
-            window.electronAPI.send("load-folder", window.electronAPI.dirname(res.filePath));
-
-            // const alreadyExists = Object.values(projects).some(
-            //     (p) => p.rootPath === basePath
-            // );
-
-            // console.log("alreadyExists: ", alreadyExists);
-            // if (!alreadyExists) {
-            //     // 通知主进程 watch 新文件夹，并触发 folder-changed
-            //     // window.electronAPI.send(basePath);
-            //     window.electronAPI.send("refresh-folder", basePath);
-            // }
-
-            // 可选：关闭这个 orphan tab，因为已经转为项目内了
-            // removeOrphanFile(current.path);
+        // ✅ 自动添加一个新项目（如果这个文件不在已有项目中）
+        window.electronAPI.send("load-folder", window.electronAPI.dirname(res.filePath));
     }
 }
--- selene-ui-react/src/useElectronEvents.ts
+++ selene-ui-react/src/useElectronEvents.ts
@@ -8,7 +8,6 @@ export function useElectronEvents() {
 
   const addProject = useProjectsStore((s) => s.addProject);
   const closeProject = useProjectsStore((s) => s.closeProject);
-  const projects = useProjectsStore((s) => s.projects);
   const updateProject = useProjectsStore((s) => s.updateProject);
 
   /**
@@ -18,6 +17,7 @@ export function useElectronEvents() {
    */
   const loadFolder = async (folder: Folder) => {
     console.log("[useElectronEvents] load-folder folder: ", folder);
+    const { projects, } = useProjectsStore.getState(); // 💥 get 最新状态
     const exists = Object.values(projects ?? {}).some(item => item.rootPath === folder.basePath);
     if (exists) {
       folderChangedHandler(folder);
@@ -32,10 +32,15 @@ export function useElectronEvents() {
    * 关闭文件夹
    */
   const closeFolderHandler = async () => {
-    const {activeProjectId} = useProjectsStore.getState(); // 💥 get 最新状态
+    const { activeProjectId } = useProjectsStore.getState(); // 💥 get 最新状态
     closeProject(activeProjectId);
   }
 
+  /**
+   * 文件夹内容变化 刷新整个文件树
+   * @param folder 
+   * @returns 
+   */
   const folderChangedHandler = async (folder: Folder) => {
     console.log("[ipcRenderer] folder-changed:", folder);
     if (!folder) {  // 文件夹为空，不处理
@@ -45,11 +50,11 @@ export function useElectronEvents() {
 
     const { basePath, contents } = folder;
 
-    const {projects,} = useProjectsStore.getState(); // 💥 get 最新状态
+    const { projects, } = useProjectsStore.getState(); // 💥 get 最新状态
 
     // 查找已存在的项目 ID（根据 basePath 匹配）
     const existingProject = Object.values(projects).find(
-        (p) => p.rootPath === basePath
+      (p) => p.rootPath === basePath
     );
 
     if (!existingProject) {
@@ -71,29 +76,26 @@ export function useElectronEvents() {
 
   // 文件保存处理
   const saveHandler = async () => {
-    console.log("[useElectronEvents] saveHandler");
-
+    console.log("[useElectronEvents] saveHandler start");
     const {
       orphanFiles,
       activeOrphanFile,
       getActiveProject,
     } = useProjectsStore.getState(); // 💥 get 最新状态
 
-    const project =  getActiveProject();
+    const project = getActiveProject();
 
-    console.log("[useElectronEvents] project: ", project, "orphanFiles: ", orphanFiles);
+    console.log("[useElectronEvents] saveHandler project: ", project, "orphanFiles: ", orphanFiles);
     const openFiles = project?.openFiles ?? orphanFiles;
 
-    console.log("[useElectronEvents] openFiles: ", openFiles);
+    console.log("[useElectronEvents] saveHandler openFiles: ", openFiles);
 
     const currentFile =
-        project?.openFiles.find((f) => f.path === project.lastActiveFile) ||
-        orphanFiles.find((f) => f.path === activeOrphanFile);
+      project?.openFiles.find((f) => f.path === project.lastActiveFile) ||
+      orphanFiles.find((f) => f.path === activeOrphanFile);
 
-    console.log("[useElectronEvents] current: ", currentFile);
-    await handleFileSave(currentFile, (oldPath, updated) => {
-
-    });
+    console.log("[useElectronEvents] saveHandler current: ", currentFile);
+    await handleFileSave(currentFile);
   };
 
   useEffect(() => {
@@ -104,57 +106,18 @@ export function useElectronEvents() {
       return;
     }
 
-
-    // ✅ 创建文件/文件夹逻辑
-    const handleNewFile = async (dirPath: string) => {
-      const res = await window.electronAPI.createFile(dirPath, "新建文件.txt");
-      if (res.success) {
-        // 简单做法：重新加载整个目录
-        window.electronAPI.send("refresh-folder", dirPath);
-      }
-    };
-
-    const handleNewFolder = async (dirPath: string) => {
-      const res = await window.electronAPI.createFolder(dirPath, "新建文件夹");
-      if (res.success) {
-        window.electronAPI.send("refresh-folder", dirPath);
-      }
-    };
-
-
-
     // 监听主程序发送的 replace-folders 事件。 刷新整个工作区域 放置 打开的文件夹
     api.on("load-folder", loadFolder); //  监听打开文件夹操作，加载文件夹
     api.on("close-folder", closeFolderHandler);
     api.on("folder-changed", folderChangedHandler);
     api.on("file-save", saveHandler);
-    
-
-    // api.on('folder-changed', (_, folderPath: string) => {
-    //   console.log("监听到文件变化，刷新文件树：", folderPath);
-    //   loadFolder(folderPath); // 重新读取并渲染文件树
-    // });
 
 
-
-    // 如果切换了项目，更新UI状态
-    // if (activeProjectId) {
-    //   const project = getActiveProject();
-    //   if (project && project.openFiles) {
-    //     console.log("[useFileSaveListener] activeProjectId: ", activeProjectId, ", project: ", project);
-    //     setOpenFiles(project.openFiles);
-    //     setActiveFile(project.lastActiveFile ?? "");
-    //   }
-    // }
 
     return () => {
       api.removeAllListeners("file-save");
       api.removeAllListeners("load-folder");
       api.removeAllListeners("folder-changed");
-      //     api.removeAllListeners("replace-folders");
-      //     api.removeAllListeners("append-folder");
-      //     api.removeAllListeners("replace-folder");
     };
-    // }, [openFiles, activeFile, setOpenFiles, setActiveFile, activeProjectId, addProject, updateProjectFiles, getActiveProject, setActiveProjectId]);
   }, []);
 }
